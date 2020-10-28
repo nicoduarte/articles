74946afe153a9b851dff62b54fd7b55b27726313
nicolasduarte <nicolas.duarte@infinixsoft.com>
2020-10-27 23:31:02 -0300

    New function insertToDb

--- app/src/main/java/com/nicoduarte/articles/ui/list/ArticleRepository.kt
+++ app/src/main/java/com/nicoduarte/articles/ui/list/ArticleRepository.kt
@@ -1,6 +1,7 @@
 package com.nicoduarte.articles.ui.list
 
 import android.app.Application
+import android.util.Log
 import com.nicoduarte.articles.api.ApiService
 import com.nicoduarte.articles.database.ArticleDao
 import com.nicoduarte.articles.database.ArticleDatabase
@@ -8,6 +9,7 @@ import com.nicoduarte.articles.database.model.Article
 import io.reactivex.Observable
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 
 class ArticleRepository(application: Application) {
@@ -26,15 +28,18 @@ class ArticleRepository(application: Application) {
             .subscribeOn(Schedulers.io())
             .map { it.hits }
             .onErrorResumeNext { _: Throwable -> getArticlesFromDb() }
-            .doOnNext {
-                articleDao.insertAll(it)
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribeOn(Schedulers.io())
-                    .subscribe({ it.size
-                    }, {
-                        it.message
-                    })
-            }
+            .doOnNext { insertToDb(it) }
+    }
+
+    private fun insertToDb(list: List<Article>): Disposable {
+        return articleDao.insertAll(list)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
+            .subscribe({
+                Log.d("Inserteds", list.size.toString())
+            }, {
+                Log.d("Error", it.message.toString())
+            })
     }
 
     private fun getArticlesFromDb(): Observable<List<Article>> {
